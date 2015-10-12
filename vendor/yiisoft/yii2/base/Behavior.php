90db2fa1d225b70cae4805a11ee022204873a8c9
litbear <litbear@foxmail.com>
2015-10-12 10:11:13 +0800

    阅读完了行为的源代码 vendor/yiisoft/yii2/base/Behavior.php

--- vendor/yiisoft/yii2/base/Behavior.php
+++ vendor/yiisoft/yii2/base/Behavior.php
@@ -25,26 +25,37 @@ class Behavior extends Object
 {
     /**
      * @var Component the owner of this behavior
+     * 一个Component 类的实例
+     * 表示事件的所有者
      */
     public $owner;
 
 
     /**
      * Declares event handlers for the [[owner]]'s events.
+     * 为事件所有者定义事件句柄
      *
      * Child classes may override this method to declare what PHP callbacks should
      * be attached to the events of the [[owner]] component.
+     * 子类也许会重写这个方法，用来规定 哪种PHP回调才能被绑定到（事件所有者，即Component）的事件上
      *
      * The callbacks will be attached to the [[owner]]'s events when the behavior is
      * attached to the owner; and they will be detached from the events when
      * the behavior is detached from the component.
+     * 当行为绑定到事件所有者上的时候，毁掉也会绑定在事件所有者上
+     * 行为解绑后，回调也随之解绑
      *
      * The callbacks can be any of the following:
+     * 回调必须是如下几种形式（没有全局函数？）：
      *
      * - method in this behavior: `'handleClick'`, equivalent to `[$this, 'handleClick']`
+     * - 本行为中的方法: 用`'handleClick'`, 等同于 `[$this, 'handleClick']`
      * - object method: `[$object, 'handleClick']`
+     * - 对象的方法 
      * - static method: `['Page', 'handleClick']`
+     * - 类的静态方法
      * - anonymous function: `function ($event) { ... }`
+     * - 匿名函数
      *
      * The following is an example:
      *
@@ -64,10 +75,14 @@ class Behavior extends Object
 
     /**
      * Attaches the behavior object to the component.
+     * 绑定定位对象到组件上
      * The default implementation will set the [[owner]] property
      * and attach event handlers as declared in [[events]].
+     * 默认的实现会设置属性owner和像enents中定义的那样绑定事件句柄
      * Make sure you call the parent implementation if you override this method.
      * @param Component $owner the component that this behavior is to be attached to.
+     * 参数$owner 是一个Component实例
+     * 该实例就是本行为所绑定的组件
      */
     public function attach($owner)
     {
@@ -79,9 +94,12 @@ class Behavior extends Object
 
     /**
      * Detaches the behavior object from the component.
+     * 从组件中解绑行为对象
      * The default implementation will unset the [[owner]] property
      * and detach event handlers declared in [[events]].
+     * 默认的逻辑会使本对象的owner属性为null，并且从绑定的组件中逐一解绑本行为中的事件句柄
      * Make sure you call the parent implementation if you override this method.
+     * 加入你在子类中重写了此方法，请确保调用了父类方法
      */
     public function detach()
     {
